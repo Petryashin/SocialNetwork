3485c8704ef3dc0937113f5990348600d8f62c32
Petryashin <petryashinsasha@mail.ru>
2022-01-27 01:26:22 +0300

    Подготовлены основные компоненты, этот коммит можно использовать, как основу для полнофункционального Web-приложения

---
+++ resources/js/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="root-container">
+        <dialog-component />
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+        },
+    }
+</script>
+<style scoped>
+.root-container{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+}
+</style>
