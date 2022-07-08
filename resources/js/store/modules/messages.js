fd333b36af7a87c7a4ebd2963138d582fc295f2f
Petryashin <petryashinsasha@mail.ru>
2022-07-08 19:12:42 +0300

    bugfix получения сообщений и прокидывания в store

--- resources/js/store/modules/messages.js
+++ resources/js/store/modules/messages.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 const messages = {
-    namespaced: true, 
-    state :{
-        messages : {},
+    namespaced: true,
+    state: {
+        messages: {},
         // выбранный ID чата
         chat_id: null
     },
-    mutations : {
-        setChatId : (state, chat_id) => state.chat_id = chat_id,
+    mutations: {
+        setChatId: (state, chat_id) => state.chat_id = chat_id,
         // setMessages: (state, data) => state.messages = data,
         setMessages: (state, data) => Vue.set(state.messages, state.chat_id, data),
         // setNewMessage: (state, message) => state.messages.push(message)
@@ -23,27 +23,28 @@ const messages = {
             Vue.set(state.messages, message.chat_id, item);
         },
     },
-    getters : {
+    getters: {
         getMessages: (state) => state.messages[state.chat_id],
         getChatId: (state) => state.chat_id
-    }, 
-    actions : {
-        getMessages ({commit,getters}, chat_id) {
+    },
+    actions: {
+        getMessages({commit, getters}, chat_id) {
             if (getters.getMessages?.hasOwnProperty(chat_id)) return
             this.$api.get(`/api/dialog/messages/${chat_id}`).then(response => {
                 commit("setChatId", chat_id);
-                commit("setMessages",response.data)
+                commit("setMessages", response.data.data)
                 console.log("getMessages")
                 console.log(response.data)
             })
         },
-        sendMessage({commit,getters}, message) {
-            commit("setNewMessage",message)
+        sendMessage({commit, getters}, message) {
+            commit("setNewMessage", message)
             // console.log("setNewMessage")
             // console.log(message)
             let chat_id = getters.getChatId
             this.$api.put(`/api/dialog/messages/${chat_id}`, message).then(response => {
-                console.log(response)})
+                console.log(response)
+            })
         }
     }
 }
